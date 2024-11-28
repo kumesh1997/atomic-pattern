9beb628473395eb6b817a4b17572e5559c51429a
TharinduKumesh <TharinduKumesh@GRS14844>
2024-11-28 17:44:24 +0530

    folder structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputField from './app/shared-components/atoms/Input/InputField';
+import Btn from './app/shared-components/atoms/Button';
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <InputField placeholder="dvjksdb" />
+      <Btn children={"sdkjbsd"} color='primary' size='lg' />
     </div>
   );
 }
---
+++ src/app/shared-components/atoms/Input/InputField.tsx
@@ -0,0 +1,44 @@
+import { colors } from "@mui/joy";
+import { Input } from "reactstrap";
+import { CSSModule } from "reactstrap/types/lib/utils";
+
+interface StyleProp {
+    width: number;
+    fontSize: number;
+    padding: number;
+  }
+  
+  interface propsTypes {
+    children?: React.ReactNode;
+    color?: string;
+    onClick?: (key:any) => void;
+    className?: string;
+    id?: string;
+    type?: 'submit' | 'reset' | 'button' | undefined;
+    outline?:boolean | undefined;
+    size?:string
+    style?:CSSModule | StyleProp
+    active?:boolean
+    disabled?:boolean
+    tag?: React.ElementType ;
+    href?:string
+    value?:string
+    as?:string;
+    name?: string
+    cssModule?: CSSModule;
+    target?: string;
+    title?:string
+    close?:boolean
+    block?:boolean
+    placeholder?: string
+  }
+
+const InputField = (props: propsTypes) => {
+    const { children, placeholder } = props;
+    return (
+      <Input placeholder={placeholder}>
+      </Input>
+    );
+  };
+  
+  export default InputField;
