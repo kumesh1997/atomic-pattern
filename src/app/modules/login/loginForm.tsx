6fd28f6619a177e7843e3abdb711071ffbc75ea4
TharinduKumesh <TharinduKumesh@GRS14844>
2024-12-02 21:35:35 +0530

    Css Added

--- src/app/modules/login/loginForm.tsx
+++ src/app/modules/login/loginForm.tsx
@@ -2,64 +2,78 @@ import { Container, Form, FormGroup } from "reactstrap";
 import Logo from "../../shared-components/atoms/Logo/CdLogo";
 import CdCard from "../../shared-components/atoms/Card/CdCard";
 import CdCardBody from "../../shared-components/atoms/Card/CardBody";
-import H1 from "../../shared-components/atoms/Headings/H1Element";
-import H5 from "../../shared-components/atoms/Headings/H5Element";
 import InputField from "../../shared-components/atoms/Input/CdInputField";
 import InputWithLabel from "../../shared-components/molecules/InputWithLable/CdInputWithLable";
 import InputPasswordField from "../../shared-components/atoms/Input/password/CdPassword";
 import Btn from "../../shared-components/atoms/Button/CdButton";
-import CdInputGroup from "../../shared-components/molecules/InputGroup/CdInputGroup";
 import Typography from "../../shared-components/atoms/Typography/Typography";
+import H6 from "../../shared-components/atoms/Headings/H6Element";
+import H3 from "../../shared-components/atoms/Headings/H3Element";
 
 const LoginForm = () => {
   return (
-    <div className="container mt-5">
-      <CdCard className="card mx-auto">
+    <Container
+      style={{
+        backgroundImage: "url('/assets/images/login_bg.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        minHeight: "100vh",
+        minWidth: "100%",
+      }}
+      className="p-5"
+    >
+      <CdCard className="rounded-3 mx-auto" style={{ width: "30rem" }}>
         <CdCardBody>
           <Container>
-            <Container>
-              <Form>
-                <Logo src="/logo192.png" style={{ width: 50, height: 50 }} />
-                <H1 children="Login to Access Department Dashboard" />
-                <H5 children="Login below to access your department dashboard." />
-                <InputWithLabel
-                  InputComponent={InputField}
-                  label="User Name"
-                  inputProps={{
-                    type: "text",
-                  }}
-                  inputPlaceHolder="username@email.com"
-                  labelProps={{
-                    className: "",
-                    style: { color: "gray", float: "left" },
-                  }}
-                />
-                <InputWithLabel
-                  InputComponent={InputPasswordField}
-                  inputPlaceHolder="Enter your Password"
-                  label="Password"
-                  type="password"
-                  labelProps={{
-                    className: "",
-                    style: { color: "gray", float: "left" },
-                  }}
-                />
-                <Btn
-                  children={"Login"}
-                  style={{ width: 100 }}
-                  color="secondary"
-                  block
-                  outline
-                  size="lg"
-                  className="mt-5"
-                />
-                <Typography children={'Forgot your username of password'} className="mt-3 text-start" />
-              </Form>
+            <Container className="mb-5">
+              <Logo
+                src="/assets/images/user.png"
+                style={{ width: 150, height: 150 }}
+              />
+              <H3 children="Login to Access Department Dashboard" />
+              <H6 children="Login below to access your department dashboard." />
             </Container>
+            <Form>
+              <InputWithLabel
+                InputComponent={InputField}
+                label="User Name"
+                inputProps={{
+                  type: "text",
+                }}
+                inputPlaceHolder="username@email.com"
+                labelProps={{
+                  className: "",
+                  style: { color: "gray", float: "left" },
+                }}
+              />
+              <InputWithLabel
+                InputComponent={InputPasswordField}
+                inputPlaceHolder="Enter your Password"
+                label="Password"
+                type="password"
+                labelProps={{
+                  className: "",
+                  style: { color: "gray", float: "left" },
+                }}
+              />
+              <Btn
+                children={"Login"}
+                style={{ width: 100 }}
+                color="secondary"
+                block
+                outline
+                size="lg"
+                className="mt-5"
+              />
+              <Typography
+                children={"Forgot your username or password?"}
+                className="mt-3 text-start"
+              />
+            </Form>
           </Container>
         </CdCardBody>
       </CdCard>
-    </div>
+    </Container>
   );
 };
 
