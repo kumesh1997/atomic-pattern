1d6dddae0ee0da1f586905de8d2b506cc6739577
TharinduKumesh <TharinduKumesh@GRS14844>
2024-12-03 14:47:16 +0530

    Css added

---
+++ src/app/modules/project/projectTable.tsx
@@ -0,0 +1,85 @@
+import { Col, Container, Row } from "reactstrap";
+import Typography from "../../shared-components/atoms/Typography/Typography";
+import InputWithLabel from "../../shared-components/molecules/InputWithLable/CdInputWithLable";
+import DropDownAtom from "../../shared-components/atoms/DropDown/CdDropDown";
+import CdInputGroup from "../../shared-components/molecules/InputGroup/CdInputGroup";
+import InputField from "../../shared-components/atoms/Input/CdInputField";
+import Icon from "../../shared-components/atoms/Icon/CdIcon";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Btn from "../../shared-components/atoms/Button/CdButton";
+import Date from "../../shared-components/atoms/Date/CdDate";
+import DataTable from "../../shared-components/organisms/Table/DataTable";
+
+const ProjectTable = () => {
+  return (
+    <Container className="container vh-100 vw-100 rounded bg-gray">
+      <Row className=" row rounded  p-2">
+        <Row className="justify-content-start p-2 mb-2">
+          <Col className="col-lg-6 col-md-7 col-sm-12 d-flex align-items-center">
+            <Typography
+              children="All Projects"
+              className="w-auto me-3 fs-5 fw-semibold"
+            />
+            <Typography children="Displaying 145554 projects" className="" />
+          </Col>
+        </Row>
+        <Row className="flex-sm-column flex-md-row flex-lg-row">
+          <Col>
+            <CdInputGroup
+              InputComponent={InputField}
+              IconComponent={Icon}
+              IconProps={{
+                icon: faMagnifyingGlass,
+              }}
+              label={"Project Name"}
+              labelProps={{style: { float: "left" } }}
+            />
+          </Col>
+          <Col>
+            <InputWithLabel
+              InputComponent={DropDownAtom}
+              label="Status"
+              options={["A", "B"]}
+              labelProps={{style: { float: "left" } }}
+            />
+          </Col>
+          <Col>
+            <InputWithLabel
+              InputComponent={DropDownAtom}
+              label="Created By"
+              options={["A", "B"]}
+              labelProps={{style: { float: "left" } }}
+            />
+          </Col>
+          <Col>
+            <InputWithLabel
+              InputComponent={Date}
+              label="Date"
+              labelProps={{ className: "", style: { float: "left" } }}
+            />
+          </Col>
+          <Col className="d-flex align-items-center justify-content-end">
+            <Btn children={"Create"} color="primary" />
+          </Col>
+        </Row>
+        <Row className="row mt-5">
+          <DataTable
+            bordered
+            hover
+            responsive
+            className="table-responsive "
+            headers={["ID", "NAME", "ROLE"]}
+            data={[
+              [1, "John Doe", "Admin"],
+              [2, "John Doe", "Admin"],
+              [3, "John Doe", "Admin"],
+              [4, "John Doe", "Admin"],
+            ]}
+          />
+        </Row>
+      </Row>
+    </Container>
+  );
+};
+
+export default ProjectTable;
--- src/app/shared-components/molecules/InputGroup/CdInputGroup.tsx
+++ src/app/shared-components/molecules/InputGroup/CdInputGroup.tsx
@@ -1,7 +1,8 @@
 import { IconProp, SizeProp } from "@fortawesome/fontawesome-svg-core";
-import { InputGroup } from "reactstrap";
+import { FormGroup, InputGroup } from "reactstrap";
 import { CSSModule } from "reactstrap/types/lib/utils";
 import { InputProps } from "reactstrap";
+import LableAtom from "../../atoms/Lable/CdLable";
 
 interface StyleProp {
     width?: number;
@@ -12,28 +13,37 @@ interface StyleProp {
 interface PropsTypes extends InputProps {
   IconComponent?: React.FC<any>;
   InputComponent: React.FC<any>;
+  label: string;
+  id?: string;
   className? : string;
   IconProps?: {
     color?: string;
     className?: string;
-    size: SizeProp;
+    size?: SizeProp;
     style?: CSSModule | StyleProp;
     active?: boolean;
     disabled?: boolean;
     name?: string;
     cssModule?: CSSModule;
     icon: IconProp;
+    labelProps?: {
+      className?: string;
+      style?: React.CSSProperties;
+    };
   };
 }
 
-const CdInputGroup : React.FC<PropsTypes> = ({IconComponent, IconProps, InputComponent, className, ...props}) => {
+const CdInputGroup : React.FC<PropsTypes> = ({IconComponent, IconProps, InputComponent, className, id, labelProps, label, ...props}) => {
     return (
-        <InputGroup className= {`border border-1 border-secondary rounded-sm p-1 ${className}`}>
+      <FormGroup>
+        <LableAtom htmlFor={id} {...labelProps} children={label} />
+        <InputGroup className= {`d-flex align-items-center border rounded ${className}`}>
         {IconComponent && (
-          <IconComponent icon={IconProps?.icon} {...IconProps} />
+          <IconComponent icon={IconProps?.icon} {...IconProps} className="ms-1 me-2" />
         )}
-        <InputComponent {...props} />
+        <InputComponent {...props} className="form-control border-0" />
       </InputGroup>
+      </FormGroup>
     );
 }
 
