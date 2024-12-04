9960be521d2eda978933594cc9af3c2a7bf7e38a
TharinduKumesh <TharinduKumesh@GRS14844>
2024-12-04 16:07:23 +0530

    user store completed, user data fetching from BE

--- src/app/store/reducers/users.slice.ts
+++ src/app/store/reducers/users.slice.ts
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import globalAppConfig from '../../config/global-app-config';
 
 type UserState = {
     isLoggedIn: boolean;
@@ -15,10 +16,11 @@ type UserState = {
   
   export const login = createAsyncThunk(
     'user/login',
-    async (credentials: { username: string; password: string }, thunkAPI) => {
+    async (credentials: { email: string; password: string }, thunkAPI) => {
       try {
-        const response = await axios.post('/api/login', credentials);
-        return response.data;
+        console.log('url ->',globalAppConfig.baseApiUrl)
+        const response = await axios.post(`${globalAppConfig.baseApiUrl}/users/login`, credentials);
+        return response.data.data;
       } catch (error) {
         return thunkAPI.rejectWithValue('Login failed');
       }
