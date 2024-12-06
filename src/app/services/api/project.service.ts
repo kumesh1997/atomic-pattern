0daaac868833c0a7dd33ecc6a747d0f8274ae367
TharinduKumesh <TharinduKumesh@GRS14844>
2024-12-06 11:11:47 +0530

    token handled to pass via Headers

--- src/app/services/api/project.service.ts
+++ src/app/services/api/project.service.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import globalAppConfig from '../../config/global-app-config';
 
+const token = localStorage.getItem('secret');
+
 interface fetchProjectsArgsTypes {
   projectName?: string | null;
   status?: string | null;
@@ -12,12 +14,11 @@ interface fetchProjectsArgsTypes {
 class ProjectService {
   static async fetchProjects(filters: fetchProjectsArgsTypes) {
     const { projectName, status, createdBy, fromDate, toDate } = filters;
-    console.log(filters)
     const url = `${globalAppConfig.baseApiUrl}/projects/paginated?page=1&limit=10&projectName=${projectName}&status=${status}&createdBy=${createdBy}`;
     try {
       const response = await axios.get(url,{
         headers: {
-          Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzMzMzg1NTA3LCJleHAiOjE3MzMzODkxMDd9.0GAKYxjMEjRYiHU2dhLopu2QWPKBVruw63Cn4DJtTY4`,
+          Authorization: token,
         },
       });
       return response.data;
